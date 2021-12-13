f1c19f2c7bfcb3002c89e70be206534c7958be86
Caroline Davis <caroline_davis@live.com>
2021-12-13 14:27:09 +1100

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Caroline Davis on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
