6c885b0da5816d61cdc1fbce4200b62603fe42f7
Caroline Davis <caroline_davis@live.com>
2021-12-14 10:31:58 +1100

    feat: Add dyanmic data to details view

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,20 +9,32 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
+        // creates a nav
         NavigationView {
+            // creates a tableview
             List(landmarks) { landmark in
-                NavigationLink(destination: LandmarkDetail()) {
+                // sending user to the details screen
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    // shows the component
                     LandmarkRow(landmark: landmark)
                 }
-
             }
             .navigationTitle("Landmarks")
         }
     }
 }
 
+// simple version to preview one device
+//        LandmarkList()
+//            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+
+
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+                   LandmarkList()
+                       .previewDevice(PreviewDevice(rawValue: deviceName))
+                       .previewDisplayName(deviceName)
+        }
     }
 }
